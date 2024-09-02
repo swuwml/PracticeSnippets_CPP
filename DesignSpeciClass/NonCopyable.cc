98dba346a5c8f7cbcadc2d5adc880befd052da77
大大怪将军 <swuwml@163.com>
2024-09-02 10:15:51 +0800

    设计一个禁止拷贝的类

---
+++ DesignSpeciClass/NonCopyable.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class NonCopyable
+{
+private:
+    NonCopyable(const NonCopyable &) = delete;
+    NonCopyable operator=(const NonCopyable &) = delete;
+
+public:
+    NonCopyable() { std::cout << "NonCopyable()" << std::endl; }
+    ~NonCopyable() { std::cout << "~NonCopyable()" << std::endl; }
+};
+
+int main()
+{
+    NonCopyable obj;
+    NonCopyable obj1(obj);
+    NonCopyable obj2 = obj;
+    return 0;
+}
